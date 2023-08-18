1f6652b5205529cb0612ab6047ff8bb7a481cbbd
Anthony Basnight <anthonyhbasnight@gmail.com>
2023-08-17 20:12:26 -0400

    why no seed work :(

--- hw3-old.c
+++ hw3-old.c
@@ -19,15 +19,16 @@ extern int total_guesses;
 extern int total_wins;
 extern int total_losses;
 extern char ** words;
-int generations = 0;
-char** dictionary;
-int num_words;
+int len_words;
 int* listeners;
 int len_listeners;
+char** dictionary;
 
 struct arguments {
+    struct sockaddr_in client;
     int sd;
     int seed;
+    int num_words;
     char* target;
 };
 
@@ -40,18 +41,20 @@ void signal_handler(int sig) {
     if (sig == SIGUSR1) {
         /* shut down the wordle server */
         printf("MAIN: SIGUSR1 rcvd; Wordle server shutting down...\n");
+        free(words);
         for (int i = 0; i < len_listeners; i++) {
             close(*(listeners + i));
         }
         free(listeners);
-        for (int i = 0; i < num_words; i++) {
-            free(*(dictionary + i));
-        }
-        free(dictionary);
+    }
+
+    printf("MAIN: valid guesses: %d\n", total_guesses);
+    printf("MAIN: win/loss: %d/%d\n", total_wins, total_losses);
+    for(int i = 0; i < words; i++){
+        printf("MAIN: word #%d: %s" , i, *(words + i));
     }
     exit(EXIT_SUCCESS);
 }
-
 
 
 void* wordle(void* arg) {
@@ -59,10 +62,19 @@ void* wordle(void* arg) {
     int n;
     char* buffer = calloc(9, 1);
     int sd = ((struct arguments *)arg)->sd;
-    char* hidden_word = ((struct arguments *)arg)->target;
+    int seed = ((struct arguments *)arg)->seed;
+    int num_words = ((struct arguments *)arg)->num_words;
+    char* hidden_word = calloc(6,1);
+    strcpy(hidden_word, ((struct arguments *)arg)->target);
+
+    printf("hidden word: %s\n", hidden_word);
+    free(((struct arguments *)arg)->target);
     free(arg);
-
-
+    // int rand_index = rand() % num_words;
+    // strcpy(hidden_word, *(words + rand_index));
+    //strcpy(hidden_word, *(words + 12));
+    
+    
     int guesses_left = 6;
     int correct = 0;
     do {
@@ -78,26 +90,32 @@ void* wordle(void* arg) {
             *(buffer + n) = '\0';   /* assume this is text/char data */
             printf( "THREAD %lu: rcvd guess: %s\n", pthread_self(), buffer );
             
+            printf("fuck1\n");
             /* make guess all lowercase letters */
             char* guess = calloc(6, sizeof(char));
             for (int i = 0; i < 5; i++) {
                 *(buffer + i) = tolower(*(buffer + i));
                 *(guess + i) = *(buffer + i);
             }
-
+            printf("what a smartie3\n");
             int found = 0;
+            int k = 0;
             /* check if valid word was given and create the correct buffer to send back */
             for ( char ** ptr = dictionary ; *ptr ; ptr++ ){
+                printf("%d, %s\n", k, *ptr);
                 if(strcmp(guess, *ptr) == 0){
                     found = 1;
                 }
+                k += 1;
             }
+
+            printf("howwww\n");
             if (found) {
                 /* valid guess*/
                 /* print out correct characters as capitals, correct but in wrong place as lowercase, otherwise - */
                 *(buffer) = 'Y';
                 
-                
+                printf("what a smartie\n");
                 char* modified = calloc(6, sizeof(char));
                 strcpy(modified, hidden_word);
                 pthread_mutex_lock( &mutex_guesses );
@@ -106,7 +124,7 @@ void* wordle(void* arg) {
                 }
                 pthread_mutex_unlock( &mutex_guesses );
                 guesses_left -= 1;
-                
+                printf("what a smartie1\n");
                 int* locked = calloc(5, sizeof(int));
                 for(int i = 0; i < 5; i++){
                     *(buffer + 3 + i) = '-';
@@ -116,6 +134,7 @@ void* wordle(void* arg) {
                         *(modified + i) = '-';
                     }
                 }
+                printf("what a smartie2\n");
                 /*         
                 
                 target: radar     -ada-
@@ -132,15 +151,16 @@ void* wordle(void* arg) {
                         correct = 0;
                     }
                 }
-
+                printf("what a smartie3\n");
                 if (correct == 1) {
-                    
+                    printf("what a smartie4\n");
                     /* they guessed it */
                     pthread_mutex_lock( &mutex_wins);   
                     {
                         total_wins += 1;   /* CRITICAL SECTION */
                     } 
                     pthread_mutex_unlock( &mutex_wins );
+                    printf("what a smartie5\n");
                     printf("THREAD %lu: sending reply: %s (%d guesses left)\n", pthread_self(), (buffer + 3), guesses_left);
                     printf("THREAD %lu: game over; word was %s!\n", pthread_self(), (buffer + 3));
                     break;
@@ -190,11 +210,11 @@ void* wordle(void* arg) {
 
     /* send appropriate signal to determine if user got it right or not */
     free(buffer);
+    free(hidden_word);
     close(sd);
     pthread_detach(pthread_self());
     return NULL;
 }
-
 
 
 int wordle_server(int argc, char** argv) {
@@ -209,6 +229,8 @@ int wordle_server(int argc, char** argv) {
 
     listeners = calloc(1, sizeof(int));
     len_listeners = 0;
+    //words = realloc(words, sizeof(char*));
+    len_words = 0;
 
     if(argc != 5) {
         fprintf(stderr, "vibe check failed\n");
@@ -218,16 +240,15 @@ int wordle_server(int argc, char** argv) {
     int listener_number = atoi(*(argv + 1)); /* port number that the server will listen on */
     int seed = atoi(*(argv + 2)); /* determines which words will be randomly selected */
     char* path = *(argv + 3); /* name of the dictionary file */
-    num_words = atoi(*(argv + 4)); /* number of words in the input file */
+    int num_words = atoi(*(argv + 4)); /* number of words in the input file */
 
-    words = realloc(words, sizeof(char*));
-    generations = 0;
-    dictionary = calloc(num_words + 1, sizeof(char *)); /* allocate the dictionary to hold num_words amount of 5 letter words */
+    char** dictionary = calloc(num_words + 1, sizeof(char*)); /* allocate the dictionary to hold num_words amount of 5 letter words */
+
     for (int i = 0; i < num_words; i++) {
-        *(dictionary + i) = calloc(6, sizeof(char));
+        *(dictionary + i) = calloc(6, 1);
     }
     
-    char* buffer = calloc(6, 1);
+    char* buffer = calloc(7, 1);
     int fd = open(path, O_RDONLY);
     if(fd == -1) {
         fprintf(stderr, "vibe check failed\n");
@@ -236,23 +257,24 @@ int wordle_server(int argc, char** argv) {
     
     for(int i = 0; i < num_words; i++) {
         int rc = read(fd, buffer, 6);
-        if(rc != 6){
+        if(rc != 6) {
             fprintf(stderr, "vibe check failed\n");
             return 69;
+        }
+        for(int j = 0; j < 5; j ++) {
+            *(buffer + j) = tolower(*(buffer + j));
         }
         *(buffer + rc - 1) = '\0';
         strcpy(*(dictionary + i), buffer);
     }
 
     free(buffer);
-    // for(int i = 0; i < num_words; i++) {
-    //     printf("%s\n", *(words + i));
-    // }
+    for(int i = 0; i < num_words; i++) {
+        printf("%s\n", *(dictionary + i));
+    }
     *(dictionary + num_words) = NULL;
 
     printf("MAIN: opened %s (%d words)\n", path, num_words);
-
-    srand(seed);
 
     printf("MAIN: seeded pseudo-random number generator with %d\n", seed);
 
@@ -284,6 +306,8 @@ int wordle_server(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
+    srand(seed);
+
     printf( "MAIN: Wordle server listening on port {%d}\n", port );
 
     /* ========================= network setup code above ==================== */
@@ -304,22 +328,27 @@ int wordle_server(int argc, char** argv) {
 
         /* call thread instead of fork */
 
-        
         // char* args = calloc(1, sizeof(arguments));
         // *args = newsd;
+
+        char* target = calloc(6, 1);
+        int rand_index = rand() % num_words;
+        printf("%d\n", rand_index);
+        strcpy(target, *(dictionary + rand_index));
+        words = realloc(words, (len_words + 1)*sizeof(char *));
+        *(words + len_words) = calloc(6, 1);
+        strcpy(*(words + len_words), target);
+        len_words += 1;
         struct arguments * args = malloc(sizeof(remote_client) + 4);
         args->sd = newsd;
-        args->seed = seed;
+        args->num_words = num_words;
         args->target = calloc(6, 1);
-        int rand_index = rand() % num_words;
-        strcpy(args->target, *(dictionary + rand_index));
-        generations += 1;
-        words = realloc(words, generations * sizeof(char*));
-        *(words + generations - 1) = calloc(6, 1);
-        strcpy(*(words + generations - 1), *(dictionary + rand_index));
+        strcpy(args->target, target);
 
         pthread_t tid;
         int rc = pthread_create(&tid, NULL, wordle, args);
+
+        free(target);
 
         if ( rc == -1 ) { 
             perror( "pthread_create() failed" ); 
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 gcc -g -Wall -Wextra -pthread hw3-main.c hw3.c
-./a.out 23 91 dictionary.txt 5757
+./a.out 27 91 dictionary.txt 5757
